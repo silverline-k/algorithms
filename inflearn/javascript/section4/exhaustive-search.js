590447552a73b680a5bd531223704f67e62be197
silverline-k <riv2rsilv2rlin2@gmail.com>
2025-10-07 03:18:35 +0900

    javascript: section4 - 뒤집은 소수

--- inflearn/javascript/section4/exhaustive-search.js
+++ inflearn/javascript/section4/exhaustive-search.js
@@ -1,8 +1,8 @@
 import { runTest } from '../../../test-helper.js';
 import fs from 'fs';
 
-const [n, arrStr] = fs.readFileSync('./inflearn/javascript/section4/input1.txt').toString().trim().split('\n');
-const arr = arrStr.split(' ').map(Number);
+const input1 = fs.readFileSync('./inflearn/javascript/section4/input1.txt').toString().trim().split('\n');
+const input2 = fs.readFileSync('./inflearn/javascript/section4/input2.txt').toString().trim().split('\n');
 
 // 1. 자릿수의 합
 // 타입 변환해서 구현할 생각하지 말고 수학적으로 생각할 것
@@ -33,4 +33,41 @@ function solution1(_, arr) {
     return answer;
 }
 
-runTest('자릿수의 합 #1', solution1, 137, n, arr);
+function isPrime(num) {
+    if (num < 2) return false;
+
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+
+    return true;
+}
+
+// 2. 뒤집은 소수
+function solution2(_, arr) {
+    const answer = [];
+
+    // 내장함수 사용해서 타입 변환 후 reverse 할 경우 0.040125 ms
+    // for (let num of arr) {
+    //     let res = Number(num.toString().split('').reverse().join(''));
+    //     if (isPrime(res)) answer.push(res);
+    // }
+
+    // 수학적으로 풀 경우 0.032958 ms
+    for (let num of arr) {
+        let rev = 0;
+
+        while (num) {
+            let tmp = num % 10;
+            rev = rev * 10 + tmp;
+            num = Math.floor(num / 10);
+        }
+
+        if (isPrime(rev)) answer.push(rev);
+    }
+
+    return answer.join(' ');
+}
+
+runTest('자릿수의 합 #1', solution1, 137, input1[0], input1[1].split(' ').map(Number));
+runTest('뒤집은 소수 #1', solution2, '23 2 73 2 3', input2[0], input2[1].split(' ').map(Number));
