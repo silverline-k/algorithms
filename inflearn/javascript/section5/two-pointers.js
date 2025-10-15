2d8ab6e9e3b6cbb40b5427d15d36941a3a16eb23
silverline-k <riv2rsilv2rlin2@gmail.com>
2025-10-15 23:56:53 +0900

    javascript: section5 - 연속 부분수열1

--- inflearn/javascript/section5/two-pointers.js
+++ inflearn/javascript/section5/two-pointers.js
@@ -3,6 +3,7 @@ import { runTest } from '../../../test-helper.js';
 
 const input1 = fs.readFileSync('./inflearn/javascript/section5/input1.txt').toString().trim().split('\n');
 const input2 = fs.readFileSync('./inflearn/javascript/section5/input2.txt').toString().trim().split('\n');
+const input3 = fs.readFileSync('./inflearn/javascript/section5/input3.txt').toString().trim().split('\n');
 
 // 1. 두 배열 합치기
 // while문 3개나 썼는데 더 좋은 방법 없는지 찾아보기
@@ -48,5 +49,33 @@ function solution2(n, m, arr1, arr2) {
     return answer.join(' ');
 }
 
+// 연속 부분수열1
+// 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구하기
+// 슬라이딩 윈도우 적용해서 구해야 할 듯
+function solution3(n, m, sequence) {
+    let answer = 0;
+    let sum = 0;
+    let start = 0;
+
+    for (let i = 0; i < n; i++) {
+        sum += sequence[i];
+
+        if (sum === m) {
+            sum -= sequence[start];
+            answer++;
+            start--;
+        } else if (sum > m) {
+            sum -= sequence[start];
+            if (sum === m) {
+                answer++;
+                start++;
+            }
+        }
+    }
+
+    return answer.toString();
+}
+
 runTest('두 배열 합치기 #1', solution1, '1 2 3 3 5 6 7 9', parseInt(input1[0]), input1[1].split(' ').map(Number), parseInt(input1[2]), input1[3].split(' ').map(Number));
 runTest('공통원소 구하기 #1', solution2, '2 3 5', parseInt(input2[0]), parseInt(input2[1]), input2[2].split(' ').map(Number), input2[3].split(' ').map(Number));
+runTest('연속 부분수열1 #1', solution3, '3', parseInt(input3[0].split(' ')[0]), parseInt(input3[0].split(' ')[1]), input3[1].split(' ').map(Number));
