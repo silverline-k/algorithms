101df67881c5058dc530344c783fe2a25de60417
silverline-k <riv2rsilv2rlin2@gmail.com>
2025-10-16 23:17:14 +0900

    javascript: section5 - 연속 부분수열2

--- inflearn/javascript/section5/two-pointers.js
+++ inflearn/javascript/section5/two-pointers.js
@@ -4,6 +4,7 @@ import { runTest } from '../../../test-helper.js';
 const input1 = fs.readFileSync('./inflearn/javascript/section5/input1.txt').toString().trim().split('\n');
 const input2 = fs.readFileSync('./inflearn/javascript/section5/input2.txt').toString().trim().split('\n');
 const input3 = fs.readFileSync('./inflearn/javascript/section5/input3.txt').toString().trim().split('\n');
+const input4 = fs.readFileSync('./inflearn/javascript/section5/input4.txt').toString().trim().split('\n');
 
 // 1. 두 배열 합치기
 // while문 3개나 썼는데 더 좋은 방법 없는지 찾아보기
@@ -49,7 +50,7 @@ function solution2(n, m, arr1, arr2) {
     return answer.join(' ');
 }
 
-// 연속 부분수열1
+// 3. 연속 부분수열1
 // 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구하기
 // 슬라이딩 윈도우 적용해서 구해야 할 듯
 function solution3(n, m, sequence) {
@@ -76,6 +77,51 @@ function solution3(n, m, sequence) {
     return answer.toString();
 }
 
+// 4. 연속 부분수열2
+// 3번 문제랑 비슷, 연속부분수열의 합이 특정숫자 M이하가 되는 경우가 몇 번 있는지 구하기
+// 일단 n개만큼 만큼 도는데 돌면서 옆에 숫자와 더하기 전에 비교하기 n+m?
+// 비교 끝나면 해당 index 제외하기
+// 조건 충족하면 윈도우 오른쪽으로 증가, 아닌 경우 left+1부터 윈도우 크기 1부터 다시 시작
+// function solution4(n, m, sequence) { // 해당 방법은 시간 복잡도 최악 O(n^2), 조건 넘길 때마다 sum초기화하고 모든 수가 작아서 m보다 훨씬 작은 경우 최악임
+//     let answer = 0;
+//     let sum = 0;
+//     let left = 0;
+//     let right = 0;
+
+//     while (left < n) {
+//         sum += sequence[right];
+
+//         if (sum <= m) {
+//             answer++;
+//             right++;
+//         } else {
+//             left++;
+//             right = left;
+//             sum = 0;
+//         }
+//     }
+
+//     return answer.toString();
+// }
+// 강의 풀이방법, 수학적으로 접근하기! O(n)
+// 더 하면서 카운팅
+function solution4(n, m, sequence) {
+    let answer = 0;
+    let sum = 0;
+    let lt = 0;
+
+    for (let rt = 0; rt < n; rt++) {
+        sum += sequence[rt];
+        while (sum > m) {
+            sum -= sequence[lt++];
+        }
+        answer += (rt - lt + 1);
+    }
+
+    return answer.toString();
+}
+
 runTest('두 배열 합치기 #1', solution1, '1 2 3 3 5 6 7 9', parseInt(input1[0]), input1[1].split(' ').map(Number), parseInt(input1[2]), input1[3].split(' ').map(Number));
 runTest('공통원소 구하기 #1', solution2, '2 3 5', parseInt(input2[0]), parseInt(input2[1]), input2[2].split(' ').map(Number), input2[3].split(' ').map(Number));
 runTest('연속 부분수열1 #1', solution3, '3', parseInt(input3[0].split(' ')[0]), parseInt(input3[0].split(' ')[1]), input3[1].split(' ').map(Number));
+runTest('연속 부분수열2 #1', solution4, '10', parseInt(input4[0].split(' ')[0]), parseInt(input4[0].split(' ')[1]), input4[1].split(' ').map(Number));
