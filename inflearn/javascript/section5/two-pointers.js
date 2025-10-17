f133e82fd40147cb7dc8f4978ea075b1c68db79f
silverline-k <riv2rsilv2rlin2@gmail.com>
2025-10-18 03:15:56 +0900

    javascript: section5 - 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)

--- inflearn/javascript/section5/two-pointers.js
+++ inflearn/javascript/section5/two-pointers.js
@@ -9,6 +9,7 @@ const input5 = fs.readFileSync('./inflearn/javascript/section5/input5.txt').toSt
 const input6 = fs.readFileSync('./inflearn/javascript/section5/input6.txt').toString().trim().split('\n');
 const input7_1 = fs.readFileSync('./inflearn/javascript/section5/input7-1.txt').toString().trim().split('\n');
 const input7_2 = fs.readFileSync('./inflearn/javascript/section5/input7-2.txt').toString().trim().split('\n');
+const input8 = fs.readFileSync('./inflearn/javascript/section5/input8.txt').toString().trim().split('\n');
 
 // 1. 두 배열 합치기
 // while문 3개나 썼는데 더 좋은 방법 없는지 찾아보기
@@ -215,6 +216,89 @@ function solution7(str1, str2) {
     return answer;
 }
 
+// 8. 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
+// S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하기
+// function solution8(s, t) {
+//     let answer = 1;
+//     const subS = new Map();
+//     const subT = new Map();
+
+//     for (let i = 0; i < t.length; i++) {
+//         if (subT.has(t[i])) subT.set(t[i], subT[t[i]] + 1);
+//         else subT.set(t[i], 1);
+
+//         if (subS.has(s[i])) subS.set(s[i], subS.get[s[i]] + 1);
+//         else subS.set(s[i], 1);
+//     }
+
+//     subT.forEach((v, k) => {
+//         if (!subS.has(k) || v !== subS.get(k)) answer = 0;
+//     });
+
+//     // s문자열 for문으로 돌면서 부분문자열 알파벳 카운팅
+//     for (let i = t.length; i < s.length; i++) {
+//         const leftValue = subS.get(s[i - t.length]);
+//         if (leftValue) {
+//             if (leftValue - 1 > 0) subS.set(s[i - t.length], leftValue - 1);
+//             else subS.delete(s[i - t.length]);
+//         }
+
+//         if (subS.has(s[i])) subS.set(s[i], subS.get(s[i]) + 1);
+//         else subS.set(s[i], 1);
+
+//         // t문자열에 a가 2개 있는 경우, s문자열 map에 이미 a가 1개 있는 경우 카운팅 되면 안 된다.
+//         let isEqual = true;
+//         for (const [key, value] of subT) {
+//             if (subT.has(key) && value !== subS.get(key)) isEqual = false;
+//         }
+
+//         if (isEqual) answer++;
+//     }
+
+//     return answer;
+// }
+// chatgpt로 최적화 한 코드
+function solution8(s, t) {
+    let answer = 0;
+    const tMap = new Map();
+    const sMap = new Map();
+    const tLen = t.length;
+
+    for (const ch of t) {
+        tMap.set(ch, (tMap.get(ch) || 0) + 1);
+    }
+
+    // tLen - 1 만큼 윈도우 구성, 밑에 반복문에서 추가하면서 비교할 것이기 때문
+    for (let i = 0; i < tLen - 1; i++) {
+        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+    }
+
+    let left = 0;
+    for (let right = tLen - 1; right < s.length; right++) {
+        sMap.set(s[right], (sMap.get(s[right]) || 0) + 1);
+
+        if (compareMaps(sMap, tMap)) answer++;
+
+        const leftChar = s[left];
+        const leftVal = sMap.get(leftChar);
+        if (leftVal === 1) sMap.delete(leftChar);
+        else sMap.set(leftChar, leftVal - 1);
+        left++;
+    }
+
+    return answer;
+}
+
+function compareMaps(map1, map2) {
+    if (map1.size !== map2.size) return false;
+
+    for (const [key, val] of map1) {
+        if (map2.get(key) !== val) return false;
+    }
+
+    return true;
+}
+
 runTest('두 배열 합치기 #1', solution1, '1 2 3 3 5 6 7 9', parseInt(input1[0]), input1[1].split(' ').map(Number), parseInt(input1[2]), input1[3].split(' ').map(Number));
 runTest('공통원소 구하기 #1', solution2, '2 3 5', parseInt(input2[0]), parseInt(input2[1]), input2[2].split(' ').map(Number), input2[3].split(' ').map(Number));
 runTest('연속 부분수열1 #1', solution3, '3', parseInt(input3[0].split(' ')[0]), parseInt(input3[0].split(' ')[1]), input3[1].split(' ').map(Number));
@@ -223,4 +307,5 @@ runTest('최대 매출 #1', solution5, '56', parseInt(input5[0].split(' ')[0]), 
 runTest('학급 회장(해쉬) #1', solution6, 'C', parseInt(input6[0]), input6[1].split(''));
 runTest('아나그램(해쉬) #1', solution7, 'YES', input7_1[0], input7_1[1]);
 runTest('아나그램(해쉬) #2', solution7, 'NO', input7_2[0], input7_2[1]);
+runTest('모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우) #1', solution8, 3, input8[0], input8[1]);
 
