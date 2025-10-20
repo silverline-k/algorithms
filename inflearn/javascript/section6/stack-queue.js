1b4fec2c8fe9b5d3287ba976e04eae7c0477e7e5
silverline-k <riv2rsilv2rlin2@gmail.com>
2025-10-21 01:07:28 +0900

    javascript: section6 - 크레인 인형뽑기(카카오 기출)

--- inflearn/javascript/section6/stack-queue.js
+++ inflearn/javascript/section6/stack-queue.js
@@ -3,6 +3,7 @@ import { runTest } from '../../../test-helper.js';
 const input1_1 = fs.readFileSync('./inflearn/javascript/section6/input1-1.txt').toString().trim().split('\n');
 const input1_2 = fs.readFileSync('./inflearn/javascript/section6/input1-2.txt').toString().trim().split('\n');
 const input2 = fs.readFileSync('./inflearn/javascript/section6/input2.txt').toString().trim().split('\n');
+const input3 = fs.readFileSync('./inflearn/javascript/section6/input3.txt').toString().trim().split('\n');
 
 // 1. 올바른 괄호
 // 문자열의 최대 길이 30
@@ -32,7 +33,7 @@ function solution2(str) {
 
     for (const ch of str) {
         if (ch === ')') {
-            while(stack.length && stack.pop()!=='(');
+            while (stack.length && stack.pop() !== '(');
         } else stack.push(ch);
     }
 
@@ -41,6 +42,34 @@ function solution2(str) {
     return answer;
 }
 
+// 3. 크레인 인형뽑기(카카오 기출)
+// 크레인 인형뽑기 게임 N x N 크기의 정사각형 격자 (30 x 30 이하)
+// 별도 바구니, 모든 인형은 1 x 1 크기의 격자 한 칸을 차지하고 아래 칸부터 쌓임
+// 바구니는 모든 인형이 들어갈 수 있는 크키라고 가정
+// 같은 모양의 인형 두 개가 연속해서 쌓이게 되면 터뜨려지고 바구니에서 사라짐
+// moves길이만큼 board탐색 시간 복잡도 O(m x n)
+function solution3(board, moves) {
+    let answer = 0;
+    const basket = [];
+
+    for (const move of moves) {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][move - 1] !== 0) {
+                if (basket.length && basket[basket.length - 1] === board[i][move - 1]) {
+                    basket.pop();
+                    answer += 2;
+                } else basket.push(board[i][move - 1]);
+
+                board[i][move - 1] = 0;
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
+
 runTest('올바른 괄호 #1', solution1, 'NO', input1_1[0]);
 runTest('올바른 괄호 #2', solution1, 'YES', input1_2[0]);
 runTest('괄호문자제거 #1', solution2, 'EFLM', input2[0]);
+runTest('크레인 인형뽑기(카카오 기출) #1', solution3, 4, JSON.parse(input3[0]), JSON.parse(input3[1]));
