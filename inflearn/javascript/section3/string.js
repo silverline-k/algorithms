9af039abc3eb0576831f0b3622b070a964676845
silverline-k <riv2rsilv2rlin2@gmail.com>
2025-10-30 23:46:30 +0900

    javascript: section3 - 숫자만 추출

--- inflearn/javascript/section3/string.js
+++ inflearn/javascript/section3/string.js
@@ -3,6 +3,7 @@ import { runTest } from '../../../test-helper.js';
 
 const input1 = fs.readFileSync('./inflearn/javascript/section3/input1.txt').toString().trim();
 const input2 = fs.readFileSync('./inflearn/javascript/section3/input2.txt').toString().trim();
+const input3 = fs.readFileSync('./inflearn/javascript/section3/input3.txt').toString().trim();
 
 // 1. 회문 문자열
 // 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열인지 확인
@@ -45,5 +46,33 @@ function solution2(str) {
     return 'YES';
 }
 
+// 3. 숫자만 추출
+// 문자와 숫자가 섞여있는 문자열에서 숫자만 순서대로 자연수로 풀력
+// *메서드 사용하지 않고 풀라고 했을 때 풀이 포함*
+function solution3(str) { // parseInt 사용x, 수학적으로 풀기
+    let answer = 0;
+
+    for (const c of str) {
+        if (!isNaN(c)) answer = answer * 10 + Number(c);
+    }
+
+    return answer;
+}
+// function solution3(str) { parseInt 사용
+//     let answer = '';
+
+//     for (const c of str) {
+//         if (!isNaN(c)) answer += c;
+//     }
+
+//     return parseInt(answer);
+// }
+// function solution3(str) { 정규표현식 사용
+//     let answer = str.replace(/[^0-9]/g, '');
+
+//     return parseInt(answer);
+// }
+
 runTest('회문 문자열 #1', solution1, 'YES', input1);
 runTest('유효한 팰린드롬 #1', solution2, 'YES', input2);
+runTest('숫자만 추출 #1', solution3, 208, input3);
