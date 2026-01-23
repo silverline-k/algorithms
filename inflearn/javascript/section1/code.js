44f2536258f0daabb36edf3d23431560b404731b
silverline-k <riv2rsilv2rlin2@gmail.com>
2026-01-23 21:05:08 +0900

    javascript: section1 - 홀수

--- inflearn/javascript/section1/code.js
+++ inflearn/javascript/section1/code.js
@@ -60,5 +60,23 @@ function solution5(arr) {
   return answer;
 }
 
+// 6. 홀수
+// input: 자연수 7개 (100보다 작고 홀수가 한 개 이상 반드시 존재)
+// output: 홀수들의 합 출력, 홀수들 중 최솟값 출력
+function solution6(arr) {
+  let sum = 0;
+  let min = 100;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      sum += arr[i];
+      if (min > arr[i]) min = arr[i];
+    }
+  }
+
+  return [sum, min].join('\n');
+}
+
 runTest('1부터 N까지 합 출력하기 #1', solution4, 21, 6);
 runTest('최솟값 구하기 #1', solution5, 2, [5, 3, 7, 11, 2, 15, 17]);
+runTest('홀수 #1', solution6, '256\n41', [12, 77, 38, 41, 53, 92, 85]);
