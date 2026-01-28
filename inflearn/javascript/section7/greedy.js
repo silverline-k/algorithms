59277a4c11fdbec6075ccfd81e8e3b2de7bac44c
silverline-k <riv2rsilv2rlin2@gmail.com>
2026-01-28 20:38:46 +0900

    javascript: section7 - 결혼식

--- inflearn/javascript/section7/greedy.js
+++ inflearn/javascript/section7/greedy.js
@@ -3,6 +3,7 @@ import { runTest } from '../../../test-helper.js';
 
 const input8_1 = fs.readFileSync('inflearn/javascript/section7/input8-1.txt').toString().trim().split('\n');
 const input8_2 = fs.readFileSync('inflearn/javascript/section7/input8-2.txt').toString().trim().split('\n');
+const input9 = fs.readFileSync('inflearn/javascript/section7/input9.txt').toString().trim().split('\n');
 
 // 8. 회의실 배정
 // 한 개의 회의실, n개의 회의를 위해 시간 겹치지 않게 최대수의 회의 찾기
@@ -29,5 +30,39 @@ function solution8(n, arr) {
     return answer;
 }
 
+// 9. 결혼식
+// 3일간의 피로연 장소 대여를 위해 친구들 N명의 참석 시간정보 받음
+// 피로연 장소에 동시에 존재하는 최대 인원수 구하기
+// 오는 시간 13, 가는 시간 15일 때 15시 정각에는 존재하지 않는다고 가정
+// input: 피로연 참석 인원수(5<=N<=100000), 각 인원의 오는 시간과 가는시간 정수형 2차원 배열(0~72)
+// output: 피로연장에 동시에 존재하는 최대 인원
+function solution9(n, times) {
+    let answer = 0;
+    const timeline = [];
+
+    // 내 풀이는 이중for문으로 돌면서 모든 경우를 확인했는데 강의 풀이처럼
+    // 타임라인 만들어서 입장시간,퇴장시간 구분해서 플마해서 구하는게 훨씬 시간복잡도 효율적이다.
+    // 간단하게 풀 수 있는 방법을 생각할 수 있도록 할 것.
+    for (const [start, end] of times) {
+        timeline.push([start, 's']);
+        timeline.push([end, 'e']);
+    }
+
+    timeline.sort((a, b) => {
+        if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+        else return a[0] - b[0];
+    });
+
+    let count = 0;
+    for (const [_, type] of timeline) {
+        if (type === 's') count++;
+        else count--;
+        answer = Math.max(answer, count);
+    }
+
+    return answer;
+}
+
 runTest('회의실 배정 #1', solution8, 3, parseInt(input8_1.shift()), input8_1.map(v => v.split(' ').map(Number)));
 runTest('회의실 배정 #2', solution8, 2, parseInt(input8_2.shift()), input8_2.map(v => v.split(' ').map(Number)));
+runTest('결혼식 #1', solution9, 2, parseInt(input9.shift()), input9.map(v => v.split(' ').map(Number)));
